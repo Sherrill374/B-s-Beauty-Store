06b2636ad9ec99b3da8996b0ef958a3d3b9d3e06
Sherrill374 <lebshe2207@students.ecpi.edu>
2026-05-11 18:18:21 -0400

    Replace 'Add to Cart' form with button
    Removed form for adding products to cart and replaced it with a button.

--- pages/products.php
+++ pages/products.php
@@ -1,4 +1,5 @@
 </> PHP
+session_start();
 
 <?php
 include '../includes/header.php';
@@ -7,17 +8,12 @@ include '../includes/header.php';
 $sql = "SELECT * FROM products";
 $result = mysqli_query($conn, $sql);
 
-echo "<h1>Products</h1>";
-
 while($row = mysqli_fetch_assoc($result)) {
 
   echo "<h3>" . $row['name'] . "</h3>";
   echo "<p>$" . $row['price'] . "</p>";
-
-  echo "<form method='post'>";
-  echo "<input type='submit' value= 'Add to Cart'>";
-  echo "</form>";
 }
 
 include '../includes/footer.php';
 ?> 
+echo "<button>Add to Cart</button>;
